04b179998c160df4a09ec0da13edeb657fd9156b
Ko-GyeongTae <kokt0203@naver.com>
2021-01-08 23:12:09 +0900

    add create info API

--- src/api/ periodic_table/createInfo/createInfo.js
+++ src/api/ periodic_table/createInfo/createInfo.js
@@ -3,9 +3,9 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
     Mutation: {
         createInfo: async(_, args) => {
-            const { atom, Subtitle, Text, From, Author="" } = args;
-            const info = await prisma.createAtom({
-                atom,
+            const { name, Subtitle, Text, From, Author = "" } = args;
+            const info = await prisma.createInfo({
+                name,
                 Subtitle,
                 Text,
                 From,
