784090c91432df385584da81cad67f954564283c
Ko-GyeongTae <kokt0203@naver.com>
2021-01-10 06:31:17 +0900

    hosting -> local

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,8 +6,10 @@ const PORT = process.env.PORT || 4000;
 const server = new GraphQLServer({ schema });
 server.express.use(logger("dev"));
 
-//server.start({ port: PORT }, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+server.start({ port: PORT }, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+/*
 server.listen({PORT: process.env.PORT || 4000}).then(({ url }) => {
     console.log(`Server ready at ${url}`);
     console.log(`Server running on port : ${PORT}`);
 }) 
+*/
